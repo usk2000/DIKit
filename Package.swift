3408d863790339935053eba11765989dde3df137
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-09-19 22:26:57 +0900

    feat: DIKitだけにした

--- Package.swift
+++ Package.swift
@@ -4,21 +4,13 @@ import PackageDescription
 let package = Package(
   name: "DIKit",
   platforms: [
-      .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
+      .macOS(.v10_11), .iOS(.v12), .tvOS(.v9), .watchOS(.v2)
   ],
   products: [
-    .executable(name: "dikitgen", targets: ["dikitgen"]),
     .library(name: "DIKit", targets: ["DIKit"]),
-    .library(name: "DIGenKit", targets: ["DIGenKit"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.0")
   ],
   targets: [
-    .target(name: "DIKit"),
-    .target(name: "DIGenKit", dependencies: ["DIKit", "SourceKittenFramework"]),
-    .target(name: "dikitgen", dependencies: ["DIGenKit"]),
-    .testTarget(name: "DIGenKitTests", dependencies: ["DIGenKit"])
+    .target(name: "DIKit")
   ],
   swiftLanguageVersions: [.v5]
 )
